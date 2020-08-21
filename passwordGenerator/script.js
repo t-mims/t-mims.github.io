adccda01ec0cb2a49884329ed54933858c9ac139
Tahliyah Mims <mimst97@gmail.com>
2020-08-21 17:01:19 -0500

    Added readme, added more function to js file, needs update for characters array

--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -1,12 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let upper=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",",Z"]
-let lower=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","s"]
-let special=["!","@","#","$","%","^","&","*","(",")","+","=","`","~","?",">","<",",",".",";"]
-let number=["0123456789"]
-let characters=["upper", "lower", "special", "number"]
+let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lower="abcdefghijklmnopqrstuvwxyz";
+let special="!@#$%^&*()+-=_:;?.";
+let number="0123456789"
+let characters=[];
 
-// Write password to the #password input
+let incUpper= confirm("Would you like to include upper case letters?")
+let incLower= confirm("Would you like to include lower case letters?")
+let incSpecial= confirm("Would you like to include special characters?")
+let incNumber= confirm("Would you like to include numbers?")
+let length= prompt("How many characters would you like in the password? (enter number between 8 and 128");
+
+function definePassword(){
+  if (8<=length<=128){
+    if (incUpper===true){
+      characters+= upper.split("");
+    }
+    else if (incLower===true){
+      characters+= lower.split("");
+
+    }
+    else if (incSpecial===true){
+      characters+= special.split("");
+     
+    }
+    else if (incNumber===true){
+      characters+= number.split("");
+    }
+  }
+  else {
+    length= prompt ("Please enter a number between 8 and 128.")
+  }
+  console.log (characters)
+  return characters
+}
+definePassword()
+word = ""
+function generatePassword(){
+
+  for ( var i=0; i<length; i++){
+     word = word + characters[(Math.floor(Math.random() * characters.length))]
+
+  console.log (word)
+    }
+  }
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -17,3 +56,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
