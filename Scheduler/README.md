d64c64faec8177170a969d450358d194b61b4116
Tahliyah Mims <mimst97@gmail.com>
2020-09-03 14:42:51 -0500

    modified classes for timeblock, pseduocoding

--- Scheduler/README.md
+++ Scheduler/README.md
@@ -16,7 +16,7 @@ GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
 -x-THEN the current day is displayed at the top of the calendar
 WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
+-x-THEN I am presented with timeblocks for standard business hours
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 WHEN I click into a timeblock
--- Scheduler/script.js
+++ Scheduler/script.js
@@ -6,9 +6,24 @@ $("#currentDay").text("Today's date is " + dayOf);
 let timeblocksDis= $(".container");
 let Hours= ["7:00 AM","8:00 AM","9:00 AM","10:00 AM","11:00 AM","12:00 PM ","1:00 PM","2:00 PM","3:00 PM","4:00 PM","5:00 PM","6:00 PM","7:00 PM"];
 for (var i=0; i< Hours.length; i++){
-    hourOf= $("<div class=row hour>");
-    timeblocksDis.append(hourOf.text(Hours[i]));
-    hourOf.append($("<input class=time-block>"));
-    hourOf.append($("<button class=saveBtn>"))
+    hourOf= $("<div class=row time-block>");
+    timeDis=$("<div class=col-md-1 hour>");
+    inputDis=$("<input class= col-md-10 past>")
+    timeblocksDis.append(hourOf);
+    hourOf.append((timeDis).text(Hours[i]));
+    hourOf.append(inputDis);
+    hourOf.append($("<button class=col-md-1 saveBtn>"))
 }
-
+//will need function that takes in current time and alters schedule appearance
+//based on the hour; current will be red/orange, past will be gray, future will be green
+// 
+//function that takes input values and saves to local storage
+function captureInput(){
+    localStorage.setItem()
+}
+//function that renders any previous input by pulling from local storage
+function renderInput(){
+    localStorage.getItem()
+}
+//calls captureInput upon pressing save button **Need to capture button 
+saveBtn.addEventListener("click",captureInput);
