e0257aedf0beab95aa66093070f8a23c9f903a62
Tahliyah Mims <mimst97@gmail.com>
2020-09-16 12:07:26 -0500

    script pseudocode & html elements and pseudocode

---
+++ Weather_API/script.js
@@ -0,0 +1,8 @@
+//call weather api (ajax,method=get).then(append to current)
+//& a for loop that appends info (city, date, weather type in pic form, temp, wind speed, humidity, UV) to #current
+//if statement for UV (if too high, highlght red, okay->green, kinda bad->yellow)
+//another for loop that iterates over 5-day length
+//appends each day info to a table(?) 
+//store each valid search into local and save to #history, added into an array
+//#history.on("click", function that will get local & display current/future )
+// for history array (var last-search===array[last/end], will automatically render if array.length>1)
